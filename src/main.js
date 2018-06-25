ed249bfe7d1064ee95bc2c2e09c8cf7c6126d42c
zl <897100187@qq.com>
2018-06-25 23:02:38 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
 
 Vue.config.productionTip = false
 
